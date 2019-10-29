fa312009015244c46060fd4fd13aec0a9ace5c92
dianynf <dianbugas@gmail.com>
2019-10-29 22:19:48 +0700

    belajar function

---
+++ membuatFungsi.js
@@ -0,0 +1,52 @@
+// buat fungsi dibawah ini
+function cetakPesan() {
+    console.log('Halo Coder!');
+}
+// panggil fungsi di bawah ini
+cetakPesan();
+
+//===========latihan============
+// buat fungsi dibawah ini
+function greetings() {
+    console.log('Selamat belajar Javascript!');
+}
+// panggil fungsi di bawah ini
+greetings();
+
+// Kita dapat membuat fungsi sendiri untuk kebutuhan yang lebih spesifik pada aplikasi kita.Berikut adalah sintaks untuk mendeklarasikan fungsi:
+
+// function namafungsi() {
+//     // blok kode fungsi
+// }
+
+
+// Membuat fungsi diawali dengan kata kunci function diikuti nama fungsinya dan kurung buka dan kurung tutup.Kemudian blok kode fungsi ditulis di dalam kurung kurawal.Nama fungsi bersifat case sensitive.
+
+// function cetakPesan() {
+//     console.log('Halo Coder!');
+// }
+// Pada contoh di atas kita membuat fungsi dengan nama cetakPesan().Fungsi tersebut memiliki satu buah baris kode di dalamnya, yakni mencetak teks bertuliskan "Halo Coder!".Blok kode di dalam fungsi tidak akan dijalankan sampai fungsi tersebut dipanggil.Cara memanggil fungsi adalah dengan menuliskan nama fungsinya diikuti oleh kurung buka dan kurung tutup.
+
+//     cetakPesan();
+
+
+// Latihan
+// Contoh kode:
+
+// // buat fungsi dibawah ini
+
+// // panggil fungsi di bawah ini
+
+
+
+// Link ke editor:
+
+// https://glot.io/snippets/fbn5muirpi/embed
+
+
+
+// Praktek
+
+// Pada editor, buatlah sebuah fungsi dengan nama greetings().Di dalam fungsi tersebut tuliskan kode untuk mencetak pesan bertuliskan "Selamat belajar Javascript!".
+
+// Di baris selanjutnya di luar fungsi, panggil fungsi tersebut dengan menuliskan nama fungsinya greetings();.Jalankan program dan cek apakah kodemu sudah berhasil menampilkan output.
